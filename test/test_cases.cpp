4228ae395e3ba3d6cad81d7aef30edba37a530e6
Michael <michaelbrockus@gmail.com>
2021-01-21 10:33:41 -0800

    add new test cases

--- test/test_cases.cpp
+++ test/test_cases.cpp
@@ -9,7 +9,10 @@
 // project since its important to test once implementation against a set
 // of common test cases
 //
+#include "roller.hpp"
 #include "game.hpp"
+#include "dice.hpp"
+
 #include <vector>
 #include <unity.h>
 
@@ -18,38 +21,133 @@
 //
 void setUp(void)
 {
-    // TODO.
 } // end of function setUp
 
 void tearDown(void)
 {
-    // TODO.
 } // end of function tearDown
 
 //
 // list of all the test cases for this project
 //
 
-static void test_rollOutput(void)
+//
+// Dice class test cases
+static void test_rollSixSides(void)
 {
-    std::vector<int> vect = {0, 0, 0};
-    TEST_ASSERT_TRUE(rollOutput(&vect));
+    Dice *die = new(Dice); // six sides by defualt
+    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
+    die->roll();
+    TEST_ASSERT_TRUE(die->getRoll() <= 6);
+    delete die;
 } // end of test case
 
-static void test_rollOutput_withNull(void)
+static void test_rollEightSides(void)
 {
-    TEST_ASSERT_FALSE(rollOutput(nullptr));
+    Dice *die = new(Dice);
+    die->setSides(8);
+    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
+    die->roll();
+    TEST_ASSERT_TRUE(die->getRoll() <= 8);
+    delete die;
 } // end of test case
 
-static void test_rollRandom(void)
+static void test_rollTenSides(void)
 {
-    std::vector<int> vect = {0, 0, 0, 0, 0, 0};
-    TEST_ASSERT_TRUE(rollRandom(&vect));
+    Dice *die = new(Dice);
+    die->setSides(10);
+    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
+    die->roll();
+    TEST_ASSERT_TRUE(die->getRoll() <= 10);
+    delete die;
 } // end of test case
 
-static void test_rollRandom_withNull(void)
+static void test_resetDie(void)
 {
-    TEST_ASSERT_FALSE(rollRandom(nullptr));
+    Dice *die = new(Dice);
+    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
+    die->roll();
+    TEST_ASSERT_TRUE(die->getRoll() <= 6);
+    die->reset();
+    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
+    delete die;
+} // end of test case
+
+
+//
+// Roller class test cases
+static void test_rollerSixSided(void)
+{
+    Roller *roller = new(Roller);
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
+    } // end for
+
+    roller->roll();
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_TRUE((die.getRoll() <= 6));
+    } // end for
+
+    delete roller;
+} // end of test case
+
+static void test_rollerEgihtSided(void)
+{
+    Roller *roller = new(Roller);
+
+    roller->useSide(8);
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
+    } // end for
+
+    roller->roll();
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_TRUE((die.getRoll() <= 8));
+    } // end for
+
+    delete roller;
+} // end of test case
+
+static void test_rollerTenSided(void)
+{
+    Roller *roller = new(Roller);
+
+    roller->useSide(10);
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
+    } // end for
+
+    roller->roll();
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_TRUE((die.getRoll() <= 10));
+    } // end for
+
+    delete roller;
+} // end of test case
+
+static void test_rollerTweentySided(void)
+{
+    Roller *roller = new(Roller);
+
+    roller->useSide(20);
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
+    } // end for
+
+    roller->roll();
+    for (auto &die : roller->result())
+    {
+        TEST_ASSERT_TRUE((die.getRoll() <= 20));
+    } // end for
+
+    delete roller;
 } // end of test case
 
 //
@@ -59,10 +157,17 @@ int main(void)
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_rollOutput);
-    RUN_TEST(test_rollOutput_withNull);
-    RUN_TEST(test_rollRandom);
-    RUN_TEST(test_rollRandom_withNull);
+    // Dice test cases
+    RUN_TEST(test_rollSixSides);
+    RUN_TEST(test_rollEightSides);
+    RUN_TEST(test_rollTenSides);
+    RUN_TEST(test_resetDie);
+
+    // Roller test cases
+    RUN_TEST(test_rollerSixSided);
+    RUN_TEST(test_rollerEgihtSided);
+    RUN_TEST(test_rollerTenSided);
+    RUN_TEST(test_rollerTweentySided);
 
     return UNITY_END();
 } // end of function main
