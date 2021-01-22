5133c417d8f0c354cc580c6fc23fa36c8a180c80
Michael <michaelbrockus@gmail.com>
2021-01-22 14:17:35 -0800

    update test cases

--- test/test_cases.cpp
+++ test/test_cases.cpp
@@ -9,7 +9,6 @@
 // project since its important to test once implementation against a set
 // of common test cases
 //
-#include "roller.hpp"
 #include "game.hpp"
 #include "dice.hpp"
 
@@ -36,9 +35,7 @@ void tearDown(void)
 static void test_rollSixSides(void)
 {
     Dice *die = new(Dice); // six sides by defualt
-    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
-    die->roll();
-    TEST_ASSERT_TRUE(die->getRoll() <= 6);
+    TEST_ASSERT_TRUE(die->roll() <= 6);
     delete die;
 } // end of test case
 
@@ -46,9 +43,7 @@ static void test_rollEightSides(void)
 {
     Dice *die = new(Dice);
     die->setSides(8);
-    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
-    die->roll();
-    TEST_ASSERT_TRUE(die->getRoll() <= 8);
+    TEST_ASSERT_TRUE(die->roll() <= 8);
     delete die;
 } // end of test case
 
@@ -56,99 +51,18 @@ static void test_rollTenSides(void)
 {
     Dice *die = new(Dice);
     die->setSides(10);
-    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
-    die->roll();
-    TEST_ASSERT_TRUE(die->getRoll() <= 10);
+    TEST_ASSERT_TRUE(die->roll() <= 10);
     delete die;
 } // end of test case
 
 static void test_resetDie(void)
 {
     Dice *die = new(Dice);
-    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
-    die->roll();
-    TEST_ASSERT_TRUE(die->getRoll() <= 6);
-    die->reset();
-    TEST_ASSERT_EQUAL_INT(0, die->getRoll());
+    TEST_ASSERT_TRUE(die->roll() <= 6);
+    TEST_ASSERT_EQUAL_INT(0, die->reset());
     delete die;
 } // end of test case
 
-
-//
-// Roller class test cases
-static void test_rollerSixSided(void)
-{
-    Roller *roller = new(Roller);
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
-    } // end for
-
-    roller->roll();
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_TRUE((die.getRoll() <= 6));
-    } // end for
-
-    delete roller;
-} // end of test case
-
-static void test_rollerEgihtSided(void)
-{
-    Roller *roller = new(Roller);
-
-    roller->useSide(8);
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
-    } // end for
-
-    roller->roll();
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_TRUE((die.getRoll() <= 8));
-    } // end for
-
-    delete roller;
-} // end of test case
-
-static void test_rollerTenSided(void)
-{
-    Roller *roller = new(Roller);
-
-    roller->useSide(10);
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
-    } // end for
-
-    roller->roll();
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_TRUE((die.getRoll() <= 10));
-    } // end for
-
-    delete roller;
-} // end of test case
-
-static void test_rollerTweentySided(void)
-{
-    Roller *roller = new(Roller);
-
-    roller->useSide(20);
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_EQUAL_INT(0, die.getRoll());
-    } // end for
-
-    roller->roll();
-    for (auto &die : roller->result())
-    {
-        TEST_ASSERT_TRUE((die.getRoll() <= 20));
-    } // end for
-
-    delete roller;
-} // end of test case
 
 //
 //  here main is used as the test runner
@@ -162,12 +76,6 @@ int main(void)
     RUN_TEST(test_rollEightSides);
     RUN_TEST(test_rollTenSides);
     RUN_TEST(test_resetDie);
-
-    // Roller test cases
-    RUN_TEST(test_rollerSixSided);
-    RUN_TEST(test_rollerEgihtSided);
-    RUN_TEST(test_rollerTenSided);
-    RUN_TEST(test_rollerTweentySided);
 
     return UNITY_END();
 } // end of function main
